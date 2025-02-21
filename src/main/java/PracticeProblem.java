4f148e5ed2b2f9a822bb97b780bb739e26aa5d12
aarooran-anankan-ics4u-03-25 <aarooran.anankan@student.tdsb.on.ca>
2025-02-21 18:20:14 +0000

    somethign

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,4 +4,25 @@ public class PracticeProblem {
 
 	}
 
+	public static int lettersToFive(String word, char letter) { //1
+		
+		int num = 0;
+		int i = 0;
+
+		while (num <= 4) {
+
+			if (word.charAt(i) == letter) {
+				num++;
+				i++;
+			}
+
+			else {
+				i++;
+			}
+
+		}
+
+		return i;
+
+	}
 }
