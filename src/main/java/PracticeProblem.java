79dccc9d1bde9ed44345a881eb3fd39b0916181a
aarooran-anankan-ics4u-03-25 <aarooran.anankan@student.tdsb.on.ca>
2025-02-25 15:57:06 +0000

    Final Assignment

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -2,27 +2,120 @@ public class PracticeProblem {
 
 	public static void main(String args[]) {
 
+		System.out.println(lettersToFive("aarooran anankan", 'a'));
+		System.out.println(countLetter("aarooran anankan", 'a'));
+		System.out.println(oddSum(2, 6));
 	}
 
 	public static int lettersToFive(String word, char letter) { //1
 		
-		int num = 0;
-		int i = 0;
+		int occ = 0; //the amount of occurences
+		int let = 0; //each letter index
 
-		while (num <= 4) {
+		
 
-			if (word.charAt(i) == letter) {
-				num++;
-				i++;
-			}
+		for (int eachLetter = 0; eachLetter < word.length(); eachLetter++) {
+			
+			while (occ < 5 && let < word.length()) {
 
-			else {
-				i++;
+				if (word.charAt(let) == letter) {
+					occ++;
+					let++;
+				}
+	
+				else {
+					let++;
+				}
+	
 			}
 
 		}
 
-		return i;
+		if (occ <= 4) {
+			let = -1;
+		}
+
+		return let;
+
+	}
+
+	public static int countLetter(String word, char letter) { //2
+
+		int occ = 0; //amount of occurences
+		int let = 0; //letter indexes
+
+		while (let < word.length()) {
+
+			if (word.charAt(let) == letter) {
+				occ++;
+				let++;
+			}
+			
+			else {
+				let++;
+			}
+
+		}
+
+		return occ;
+
+	}
+
+	public static int oddSum(int num1, int num2) { //3
+
+		int sum = 0;
+		int une;
+		int deux;
+
+		if (num1 < num2) {
+
+			une = num1; //2
+			deux = num2; //6
+
+			for (int i=num1; i<=num2; i++) { //=2, <=6
+
+				if (une%2==0) { //if even
+					une++;
+				}
+
+				else { //if odd
+					sum = sum + une;
+					une++;
+				}
+
+			}
+
+		}
+
+		else if (num1 > num2) {
+
+			une = num1;
+			deux = num2;
+
+			for (int i=num2; i<=num1; i++) {
+
+				if (deux%2==0) { //if even
+					deux++;
+				}
+
+				else { //if odd
+					sum = sum + deux;
+					deux++;
+				}
+
+			}
+
+		}
+
+		else { //when they are equal
+			
+			if (num1%2!=0) { //if even
+				sum = num1;
+			}
+
+		}
+
+		return sum;
 
 	}
 }
